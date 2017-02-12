6458dd9b797eaf52697719709b7e4eb7e03bf98a
bryanhays2002 <bryan_hays2002@ymail.com>
2017-02-12 02:52:36 -0700

    Add files via upload

---
+++ docs/Module-4-Solution/SpeakHello.js
@@ -0,0 +1,12 @@
+(function (window) {
+
+    var helloSpeaker = {
+        speak: function(name) {
+            var speakWord = "Hello ";
+            console.log(speakWord + name);
+        }
+    };
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
